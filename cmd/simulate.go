14f5c25a601203a5b53e91c8c34123a858d7a01f
zhijie wang <zhijie.wang@accenture.com>
2019-10-30 22:46:32 -0700

    rework Observer, needs to be more of channel communication style concurrency

--- cmd/simulate.go
+++ cmd/simulate.go
@@ -1,16 +1,18 @@
-// Copyright © 2018 Zhijie (Bill) Wang <wangzhijiebill@gmail.com>
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+ *  Copyright (c) 2019 Zhijie (Bill) Wang
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package cmd
 
@@ -47,11 +49,16 @@ to quickly create a Cobra application.`,
 		fmt.Printf("Simulation took %s for %v iterations \n", elapsed, Iterations)
 	},
 }
-type BasicObserver struct {
 
+type BasicObserver struct {
 }
-func (b *BasicObserver) OnNotify(data interface{}){
-	log.Printf("%+v",data )
+
+func (b *BasicObserver) Notify(data interface{}) {
+	log.Printf("%+v", data)
+}
+
+func (b *BasicObserver) GetChannel() chan interface{} {
+	return nil
 }
 
 // Iterations is the flag for how many iterations to run
